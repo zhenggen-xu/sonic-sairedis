ebf45d66db3fe696ab6253a511a763a041776a1a
Kamil Cudnik <kcudnik@gmail.com>
2017-09-14 17:02:31 -0700

    [syncd]: Refactor mutexes (#221)

--- syncd/syncd_counters.cpp
+++ syncd/syncd_counters.cpp
@@ -29,8 +29,10 @@ void collectCountersThread(
 
         for (auto sw: switches)
         {
+            std::lock_guard<std::mutex> lock(g_mutex);
+
             /*
-             * Collect counters should be under mutex sice configuration can
+             * Collect counters should be under mutex since configuration can
              * change and we don't want that during counters collection.
              */
 
