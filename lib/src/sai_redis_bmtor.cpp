1b0d609c6f9acd1cb868898f019eaade071c85ee
Marian Pritsak <marianp@mellanox.com>
2019-04-24 10:55:08 +0300

    [SAI] Advance submodule to v1.4.1 (#450)
    Signed-off-by: Marian Pritsak <marianp@mellanox.com>

--- lib/src/sai_redis_bmtor.cpp
+++ lib/src/sai_redis_bmtor.cpp
@@ -82,6 +82,47 @@ sai_status_t redis_clear_table_bitmap_router_entry_stats(
 
 REDIS_GENERIC_QUAD(TABLE_BITMAP_ROUTER_ENTRY,table_bitmap_router_entry);
 
+sai_status_t redis_get_table_meta_tunnel_entry_stats(
+        _In_ sai_object_id_t table_meta_tunnel_entry_id,
+        _In_ uint32_t number_of_counters,
+        _In_ const sai_stat_id_t *counter_ids,
+        _Out_ uint64_t *counters)
+{
+    MUTEX();
+
+    SWSS_LOG_ENTER();
+
+    return SAI_STATUS_NOT_IMPLEMENTED;
+}
+
+sai_status_t redis_get_table_meta_tunnel_entry_stats_ext(
+        _In_ sai_object_id_t table_meta_tunnel_entry_id,
+        _In_ uint32_t number_of_counters,
+        _In_ const sai_stat_id_t *counter_ids,
+        _In_ sai_stats_mode_t mode,
+        _Out_ uint64_t *counters)
+{
+    MUTEX();
+
+    SWSS_LOG_ENTER();
+
+    return SAI_STATUS_NOT_IMPLEMENTED;
+}
+
+sai_status_t redis_clear_table_meta_tunnel_entry_stats(
+        _In_ sai_object_id_t table_meta_tunnel_entry_id,
+        _In_ uint32_t number_of_counters,
+        _In_ const sai_stat_id_t *counter_ids)
+{
+    MUTEX();
+
+    SWSS_LOG_ENTER();
+
+    return SAI_STATUS_NOT_IMPLEMENTED;
+}
+
+REDIS_GENERIC_QUAD(TABLE_META_TUNNEL_ENTRY,table_meta_tunnel_entry);
+
 const sai_bmtor_api_t redis_bmtor_api = {
 
     REDIS_GENERIC_QUAD_API(table_bitmap_classification_entry)
@@ -95,4 +136,10 @@ const sai_bmtor_api_t redis_bmtor_api = {
     redis_get_table_bitmap_router_entry_stats,
     redis_get_table_bitmap_router_entry_stats_ext,
     redis_clear_table_bitmap_router_entry_stats,
+
+    REDIS_GENERIC_QUAD_API(table_meta_tunnel_entry)
+
+    redis_get_table_meta_tunnel_entry_stats,
+    redis_get_table_meta_tunnel_entry_stats_ext,
+    redis_clear_table_meta_tunnel_entry_stats,
 };
--- vslib/src/sai_vs_bmtor.cpp
+++ vslib/src/sai_vs_bmtor.cpp
@@ -5,10 +5,14 @@ VS_GENERIC_QUAD(TABLE_BITMAP_CLASSIFICATION_ENTRY,table_bitmap_classification_en
 VS_GENERIC_STATS(TABLE_BITMAP_CLASSIFICATION_ENTRY,table_bitmap_classification_entry);
 VS_GENERIC_QUAD(TABLE_BITMAP_ROUTER_ENTRY,table_bitmap_router_entry);
 VS_GENERIC_STATS(TABLE_BITMAP_ROUTER_ENTRY,table_bitmap_router_entry);
+VS_GENERIC_QUAD(TABLE_META_TUNNEL_ENTRY,table_meta_tunnel_entry);
+VS_GENERIC_STATS(TABLE_META_TUNNEL_ENTRY,table_meta_tunnel_entry);
 
 const sai_bmtor_api_t vs_bmtor_api = {
     VS_GENERIC_QUAD_API(table_bitmap_classification_entry)
     VS_GENERIC_STATS_API(table_bitmap_classification_entry)
     VS_GENERIC_QUAD_API(table_bitmap_router_entry)
     VS_GENERIC_STATS_API(table_bitmap_router_entry)
+    VS_GENERIC_QUAD_API(table_meta_tunnel_entry)
+    VS_GENERIC_STATS_API(table_meta_tunnel_entry)
 };
